2edd09c3a27211721ec2910329981c4eed505889
ExtazyDeveloper2 <i.am31827@gmail.com>
2025-08-30 20:44:00 +0200

    Update apply-migration.js

--- server/apply-migration.js
+++ server/apply-migration.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const { db, run } = require('./db');
+const sqlite3 = require('sqlite3').verbose();
 
 async function applyMigration() {
+  let db;
+  
   try {
     console.log('Применяю миграцию для типов местности и вездеходов...');
+    
+    // Создаем новое подключение к базе данных
+    const dbPath = path.join(__dirname, 'db.sqlite3');
+    db = new sqlite3.Database(dbPath);
     
     // Читаем файл миграции
     const migrationPath = path.join(__dirname, 'migrations', '003_terrain_vehicle_types.sql');
@@ -20,23 +26,31 @@ async function applyMigration() {
     for (const statement of statements) {
       if (statement.trim()) {
         console.log('Выполняю:', statement.substring(0, 50) + '...');
-        await run(db, statement);
+        await new Promise((resolve, reject) => {
+          db.run(statement, (err) => {
+            if (err) reject(err);
+            else resolve();
+          });
+        });
       }
     }
     
     console.log('Миграция успешно применена!');
     
-    // Проверяем, что таблицы созданы
-    const terrainTypes = await db.all("SELECT * FROM terrain_types");
-    const vehicleTypes = await db.all("SELECT * FROM vehicle_types");
-    
-    console.log('Типы местности:', terrainTypes.map(t => t.name));
-    console.log('Типы вездеходов:', vehicleTypes.map(v => v.name));
-    
   } catch (error) {
     console.error('Ошибка при применении миграции:', error);
+    process.exit(1);
   } finally {
-    db.close();
+    if (db) {
+      db.close((err) => {
+        if (err) {
+          console.error('Ошибка при закрытии базы данных:', err);
+        }
+        process.exit(0);
+      });
+    } else {
+      process.exit(0);
+    }
   }
 }
 
